4b5192e0a23666bbf3e3d2bc84f02785d601dca1
187jjay187 <187jjay187@gmail.com>
2023-01-06 00:09:32 +0200

    implement toggle function and add styling to badge

--- src/Redux/utils/index.js
+++ src/Redux/utils/index.js
@@ -1,6 +1,6 @@
 export default function pick(obj, arr) {
   // eslint-disable-next-line
-    return arr.reduce((acc, record) => (record in obj && (acc[record] = obj[record]), acc), {});
+  return arr.reduce((acc, record) => (record in obj && (acc[record] = obj[record]), acc), {});
 }
 
 export const addReserved = (arr) => {
