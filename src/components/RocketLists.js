9622d96d11614eb57dbef9d1c7c40b6169702296
187jjay187 <187jjay187@gmail.com>
2023-01-06 16:23:41 +0200

    Add linter error fix and Live Demo Link

--- src/components/RocketLists.js
+++ src/components/RocketLists.js
@@ -1,13 +1,25 @@
+/* eslint-disable camelcase */
 // import react and RocketLists.css stylesheet
 import React from 'react';
+import PropTypes from 'prop-types';
+
+// Stylesheet
 import './RocketLists.css';
 
 // create a functional component called RocketLists
-const RocketLists = () => (
-  <div className="rocket-main-container">
-    <span className="rocket-name">Falcon 9</span>
-  </div>
-);
+const RocketLists = ({ rocket }) => {
+  const { rocket_name } = rocket;
+
+  return (
+    <div className="rocket-main-container">
+      <span className="profile-rocket-name">{ rocket_name }</span>
+    </div>
+  );
+};
+
+RocketLists.propTypes = {
+  rocket: PropTypes.oneOfType([PropTypes.object]).isRequired,
+};
 
 // export RocketLists
 export default RocketLists;
